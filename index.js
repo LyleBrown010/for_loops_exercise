047c743a6838df765a7d4afe80995b4303d5a62f
LyleBrown010 <lylebrown010@gmail.com>
2023-05-22 16:32:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Task 1
+for (let i = 0; i <= 20; i++){
+    if (i % 2 === 0){
+        document.write(i + " " + "is even" + "<br/>");
+    } else {
+        document.write(i + " " + "is odd" + "<br/>" );
+    }
+} 
+
+// Task 3
+function sum(){
+
+}
+
+// Task 4 
